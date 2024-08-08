934be20d38940100e5eb28e99365c1f9351be31c
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 16:40:02 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Task 1: Log a greeting message, "Script running" to the console.
+console.log("script running")
+
+// Task 2: Prompt the user to enter their username and save it in a variable called 'userName'. Then, console log "Hello, [userName]!"
+let userName=prompt("enter your name")
+console.log("Hello" + userName)
+
+
+
+// Task 3: Declare a variable named 'age' and assign it your age. Then, log a message to the console saying "I'm [age] years old."
+let age=17
+console.log("Im" + age +"years old")
+
+// Task 4: Create an object named 'class' with properties such as 'className', 'teacher', and 'grade'. Assign appropriate values to these properties.
+ let Class1 ={
+   className:"math",techer:"idk",
+   garde:89
+   
+     
+ };
+
+
+// Task 5: Console log "I'm taking [className] with [teacher]"
+console.log(`im taking ${myclass.className}with ${myClass.teacher}`)
+
+
+
+// Task 6: Write an if/else statement that checks if the grade is equal to or over 63. If it is, print "I am passing", else print "Oh no, I'm not passing YET."
+if(myClass.grade >= 63){
+  console.log("i am passing")
+}else{
+  console.log("oh no,im not passing yet")
+}
+
+
